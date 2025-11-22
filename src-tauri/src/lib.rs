e8ab4cbbde0c669151f9b40d717b915fb127f42b
Rehatbir Singh <rehatbir.singh@gmail.com>
2025-11-22 16:30:17 -0600

    feat: creating and joining rooms

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,35 +1,109 @@
+use std::str::FromStr;
+
 use iroh::{protocol::Router, Endpoint};
+use iroh_gossip::{Gossip, TopicId};
+use iroh_tickets::Ticket;
+use room_ticket::RoomTicket;
 use tauri::{Manager, State};
 use tokio::sync::RwLock;
+
+mod room_ticket;
 
 #[derive(Default)]
 struct AppStateInner {
     endpoint: Option<Endpoint>,
+    gossip: Option<Gossip>,
     router: Option<Router>,
+    gossip_send: Option<iroh_gossip::api::GossipSender>,
+    gossip_recv: Option<iroh_gossip::api::GossipReceiver>,
+    username: Option<String>,
 }
 type AppState = RwLock<AppStateInner>;
 
 #[tauri::command]
-async fn create_room(app_state: State<'_, AppState>) -> Result<(), String> {
+async fn create_room(app_state: State<'_, AppState>, username: String) -> Result<String, String> {
     let mut app_state = app_state.write().await;
 
+    // Create Iroh machinery
     let endpoint = Endpoint::builder()
         .bind()
         .await
         .map_err(|e| e.to_string())?;
-    let router = Router::builder(endpoint.clone()).spawn();
+    let gossip = Gossip::builder().spawn(endpoint.clone());
+    let router = Router::builder(endpoint.clone())
+        .accept(iroh_gossip::ALPN, gossip.clone())
+        .spawn();
 
-    // TODO: bind protocols
+    // Generate a topic ID and join the topic
+    let topic_id = {
+        let mac_addr = mac_address::get_mac_address()
+            .map_err(|e| e.to_string())?
+            .ok_or("Device MAC address not found")?
+            .to_string();
+        mac_addr.replace(":", "")
+    };
+    let topic = gossip
+        .subscribe_and_join(TopicId::from_str(&topic_id).unwrap(), vec![]) // doesn't need bootstrap peers
+        .await
+        .map_err(|e| e.to_string())?;
+    let (sender, receiver) = topic.split();
 
-    app_state.endpoint = Some(endpoint);
+    // Set state
+    app_state.endpoint = Some(endpoint.clone());
+    app_state.gossip = Some(gossip);
     app_state.router = Some(router);
-    Ok(())
+    app_state.gossip_send = Some(sender);
+    app_state.gossip_recv = Some(receiver);
+    app_state.username = Some(username);
+
+    let ticket = RoomTicket {
+        topic_id: topic_id,
+        creator_addr: endpoint.addr(),
+    };
+    Ok(ticket.serialize())
 }
 
 #[tauri::command]
-async fn join_room(app_state: State<'_, AppState>, ticket: String) -> Result<(), String> {
-    // TODO: join room
-    todo!()
+async fn join_room(
+    app_state: State<'_, AppState>,
+    username: String,
+    ticket: String,
+) -> Result<(), String> {
+    // Parse ticket
+    let room_ticket = RoomTicket::deserialize(&ticket).map_err(|e| e.to_string())?;
+
+    let mut app_state = app_state.write().await;
+
+    // Create Iroh machinery
+    let endpoint = Endpoint::builder()
+        .bind()
+        .await
+        .map_err(|e| e.to_string())?;
+    let gossip = Gossip::builder().spawn(endpoint.clone());
+    let router = Router::builder(endpoint.clone())
+        .accept(iroh_gossip::ALPN, gossip.clone())
+        .spawn();
+
+    // Join the topic
+    let topic_id = room_ticket.topic_id;
+    let topic = gossip
+        .subscribe(
+            TopicId::from_str(&topic_id).unwrap(),
+            vec![room_ticket.creator_addr.id],
+        )
+        .await
+        .map_err(|e| e.to_string())?;
+    let (sender, receiver) = topic.split();
+
+    // Set state
+    app_state.endpoint = Some(endpoint.clone());
+    app_state.gossip = Some(gossip);
+    app_state.router = Some(router);
+    app_state.gossip_send = Some(sender);
+    app_state.gossip_recv = Some(receiver);
+    app_state.username = Some(username);
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
