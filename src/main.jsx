54cd7aef36ae25f13125f9cb56017d50d05a9355
Advait Namboodiri <nambooadi@gmail.com>
2025-11-22 15:07:08 -0600

    made it matte black

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";  // or "./App"
-import "./App.css";           // keep your global styles if you want them
+import App from "./App.jsx";  
+import "./App.css";          
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
