b79233a0096f7bb3ace879713cb804db6ac1ad53
Rehatbir Singh <rehatbir.singh@gmail.com>
2025-11-23 09:37:56 -0600

    feat: no longer updating ticket when joining

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -156,19 +156,6 @@ async fn join_room(
         .map_err(|e| e.to_string())?;
     let (sender, receiver) = topic.split();
 
-    // Update remote ticket with current endpoint address
-    let mut new_ticket = ticket.clone();
-    new_ticket.current_peers.push(endpoint.addr());
-    let serialized_new_ticket = new_ticket.serialize();
-    sqlx::query!(
-        "UPDATE join_codes SET ticket = $1 WHERE code = $2",
-        serialized_new_ticket,
-        join_code
-    )
-    .execute(&mut conn)
-    .await
-    .map_err(|e| e.to_string())?;
-
     // Set state
     app_state.endpoint = Some(endpoint.clone());
     app_state.gossip = Some(gossip);
